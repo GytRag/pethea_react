acb21860d749e8013b70ce805790999937a07333
GytRag <gytis.raginskas@gmail.com>
2025-04-29 16:13:55 +0300

    Update IndexPage.js

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -36,7 +36,7 @@ const IndexPage = () => {
                     }
                 }
             })
-    }, [screenWidth])
+    }, [])
 
     useEffect(() => {
         const logScreenSize = () => {
